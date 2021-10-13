1365155cc5976fc0a6e209c737e098e7acf770fb
Danilo Martins <danimarts.dm@gmail.com>
2021-10-13 22:45:20 +0200

    Make rectangular.py work with rounded corners

--- bayes_race/tracks/play.py
+++
@@ -1,9 +0,0 @@
-
-from bayes_race.tracks import Rectangular
-from bayes_race.raceline import randomTrajectory
-import matplotlib.pyplot as plt
-
-rec = Rectangular(7, 15, 2, 1.5)
-
-fig = rec.plot(plot_centre=False)
-plt.show()
